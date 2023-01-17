930797d4fd9f4331a814fc554efd4da9e96ca5e9
Tushar Raihan <sheikhhasinur69@gmail.com>
2023-01-17 22:55:02 +0600

    complete

---
+++ main.js
@@ -0,0 +1,54 @@
+let form = document.getElementById('form');
+let input = document.getElementById('input');
+let msg = document.getElementById('msg');
+let posts = document.getElementById('posts');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    formValidation();
+})
+
+
+let formValidation = () =>{
+    if(input.value === ""){
+        console.log("failure state");
+        msg.innerHTML ="Posts cannot be blank"
+    } else{
+        console.log("success");
+        msg.innerHTML = '';
+        acceptData();
+    }
+}
+
+
+let data = {};
+
+let acceptData =()=>{
+    data["text"]= input.value;
+    console.log(data);
+    createPost();
+}
+
+let createPost =()=>{
+    posts.innerHTML += `
+    <div>
+                    <p>${data.text}</p>
+                    <span class="options">
+                        <i onClick="editPost(this)" class="fa-solid fa-pen-to-square"></i>
+                        <i onClick="deletePost(this)" class="fa-sharp fa-solid fa-trash"></i>
+                    </span>
+                </div>
+    `;
+
+    input.value='';
+}
+
+let deletePost =(e)=>{
+    e.parentElement.parentElement.remove();
+}
+
+let editPost =(e)=>{
+   input.value =  e.parentElement.previousElementSibling.innerHTML;
+   e.parentElement.parentElement.remove();
+}
+
